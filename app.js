dc79bf4e90d4d4a1aed76761716a5672722b8184
hsien7902 <yuck7902@gmail.com>
2022-05-03 15:39:52 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -2,11 +2,19 @@ const express = require('express')
 const app = express()
 const Port = 3000
 
+//載入handlebars
+const exphbs = require('express-handlebars')
+
+//設定模板引擎預設介面
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
 //routes
 
 app.get('/', (req, res) => {
-  res.send('Hello fucking world!!!')
+  res.render('index')
 })
+
 
 //listen app
 
